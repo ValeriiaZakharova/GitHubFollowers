4d4239f4988262abae95991be68c95e039557653
Valeriia Zakharova <zakh.valeriia@gmail.com>
2021-03-02 16:14:29 +0200

    Configures collection view

--- GitHubFollowers/Screens/FollowersListViewController.swift
+++ GitHubFollowers/Screens/FollowersListViewController.swift
@@ -10,28 +10,44 @@ import UIKit
 class FollowersListViewController: UIViewController {
 
     var username: String?
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
-
-    NetworkManager.shared.getFollowers(for: username!, page: 1) { result in
-
-        switch result {
-        case .success(let followers):
-            print("Followers.count = \(followers.count)")
-            print(followers)
-
-        case .failure(let error):
-            self.presentAlertViewController(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "ok")
-            }
-        }
+        configureViewController()
+        configureCollectionview()
+        getfollowers()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func configureCollectionview() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+
+    func getfollowers() {
+        NetworkManager.shared.getFollowers(for: username!, page: 1) { result in
+
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+
+            case .failure(let error):
+                self.presentAlertViewController(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "ok")
+            }
+        }
     }
 }
