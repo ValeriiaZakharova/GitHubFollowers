1a90cf07fdc180cc1566e8768618e75abe4bdb58
Valeriia Zakharova <zakh.valeriia@gmail.com>
2021-03-09 23:24:00 +0200

    Creates PersistenceManager to save/remove a specific user to/from UserDefaults

---
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,76 @@
+//
+//  PersistenceManager.swift
+//  GitHubFollowers
+//
+//  Created by Valeriia Zakharova on 09.03.2021.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager {
+    enum Keys {
+        static let favorites = "favorites"
+    }
+
+    static private let defaults = UserDefaults.standard
+
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completion: @escaping(ErrorMessage?) -> Void) {
+        retriveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+
+                switch actionType {
+                case .add:
+                    //check if retrievedFavorites contains favorite
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completion(.alreadyInFavorites)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    //itereing throw an array and if element's login equals favorite.login we remove it
+                    retrievedFavorites.removeAll { $0.login == favorite.login }
+                }
+                completion(save(favorites: retrievedFavorites))
+
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+
+    //decoding
+    static func retriveFavorites(completion: @escaping (Result<[Follower], ErrorMessage>) -> Void) {
+        //get favorites data from userdefoults if its not there-we did't save it yet, returns an empty array
+        //swift knows there is object but he did't know what type, we nned to cast it to Date
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completion(.success([]))
+            return
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completion(.success(favorites))
+        } catch {
+            completion(.failure(.unableToFavorite))
+        }
+    }
+
+    //encoding
+    static func save(favorites: [Follower]) -> ErrorMessage? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.setValue(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+}
--- GitHubFollowers/Screens/FollowersListViewController.swift
+++ GitHubFollowers/Screens/FollowersListViewController.swift
@@ -124,7 +124,32 @@ private extension FollowersListViewController {
     }
 
     @objc func addButtonTapped() {
-        print("add button tapped")
+        showLoadingView()
+
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+
+            switch result {
+            case .success(let user):
+                //create follower object to get a login and an avatarUrl for fovorite user
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+
+                //add the user to the favorite, save to the userDefoults
+                //if the error is nil we are gonna present success
+                PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let error = error else {
+                        self?.presentAlertViewController(title: "Success!", message: "You have succesfully favorited this user 🎉", buttonTitle: "Ok")
+                        return
+                    }
+                    //if error is not nil
+                    self?.presentAlertViewController(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+
+            case .failure(let error):
+                self.presentAlertViewController(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
 
