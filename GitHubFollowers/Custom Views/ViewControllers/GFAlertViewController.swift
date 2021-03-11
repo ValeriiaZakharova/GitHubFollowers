01df72ef0c0a89387580ee6457fc6c150be12f39
Valeriia Zakharova <zakh.valeriia@gmail.com>
2021-03-11 10:00:31 +0200

    Refactors GFAlertViewController

--- GitHubFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
@@ -14,10 +14,10 @@ class GFAlertViewController: UIViewController {
         static let padding: CGFloat = 20
     }
 
-    let containerView = UIView()
-    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
-    let messageLabel = GFBodyLabel(textAlignment: .center)
-    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    private let containerView = AlertContainerView()
+    private let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    private let messageLabel = GFBodyLabel(textAlignment: .center)
+    private let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
 
     var alertTitle: String?
     var message: String?
@@ -36,63 +36,53 @@ class GFAlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
-        configureContainerView()
-        configureTitleLabel()
-        configureActionButton()
-        configureMessageLabel()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        setupUI()
+    }
+}
+
+// MARK: - Private
+private extension GFAlertViewController {
+    func setupUI() {
+        setupViewHierarhy()
+        setupContent()
+        setupConstraints()
     }
 
-    func configureContainerView() {
+    func setupViewHierarhy() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(actionButton)
+        containerView.addSubview(messageLabel)
+    }
 
+    func setupContent() {
+        titleLabel.text = alertTitle ?? "Something went wrong"
+
+        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+
+        messageLabel.text = message ?? "Unable to complete request"
+        messageLabel.numberOfLines = 4
+    }
+
+    func setupConstraints() {
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             containerView.widthAnchor.constraint(equalToConstant: 280),
-            containerView.heightAnchor.constraint(equalToConstant: 220)
-        ])
-    }
+            containerView.heightAnchor.constraint(equalToConstant: 220),
 
-    func configureTitleLabel() {
-        containerView.addSubview(titleLabel)
-        titleLabel.text = alertTitle ?? "Something went wrong"
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: Constants.padding),
             titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Constants.padding),
             titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -Constants.padding),
-            titleLabel.heightAnchor.constraint(equalToConstant: 28)
-        ])
-    }
+            titleLabel.heightAnchor.constraint(equalToConstant: 28),
 
-    func configureActionButton() {
-        containerView.addSubview(actionButton)
-        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
-        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
             actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -Constants.padding),
             actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Constants.padding),
             actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -Constants.padding),
-            actionButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
-    }
+            actionButton.heightAnchor.constraint(equalToConstant: 44),
 
-    func configureMessageLabel() {
-        containerView.addSubview(messageLabel)
-        messageLabel.text = message ?? "Unable to complete request"
-        messageLabel.numberOfLines = 4
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
             messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Constants.padding),
             messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -Constants.padding),
